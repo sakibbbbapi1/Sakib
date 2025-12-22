30787a0cf1951b34c21321e22d1f8b9f85704cce
sakibbbbapi1 <sakibbb3.00@gmail.com>
2025-12-22 18:46:52 +0600

    Update api.js

--- js /api.js
+++ js /api.js
@@ -1,1 +1,16 @@
+// Add Friend
+async function addFriend(guestUid, guestPassword, targetUid) {
+  const url = https://danger-add-friend.vercel.app/adding_friend?uid=${guestUid}&password=${guestPassword}&friend_uid=${targetUid};
+  const res = await fetch(url);
+  return res.json();
+}
 
+// Remove Friend
+async function removeFriend(guestUid, guestPassword, targetUid) {
+  const url = https://danger-add-friend.vercel.app/remove_friend?uid=${guestUid}&password=${guestPassword}&friend_uid=${targetUid};
+  const res = await fetch(url);
+  return res.json();
+}
+
+// Later you can add:
+// Guild Join, Ban Check, Region Check, Guild Info, JWT Mode functions
